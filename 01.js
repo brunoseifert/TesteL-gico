1ee9a6448e99347c76ec5499583884f1bb424b44
Bruno Seifert <128076381+brunoseifert@users.noreply.github.com>
2024-03-08 15:45:44 -0300

    feat: inital commit

---
+++ 01.js
@@ -0,0 +1,15 @@
+let INDICE = 13,
+  SOMA = 0,
+  K = 0;
+
+while (K < INDICE) {
+  K = K + 1;
+  SOMA = SOMA + K;
+}
+
+console.log(SOMA);
+
+// o resultado será 91
+// o código está somando todos os números de 1 a 13
+// 1+2+3+4+5+6+7+8+9+10+11+12+13 = 91
+// para ver o resultado basta rodar o código com o comando no terminal "node 01"
