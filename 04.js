b585d28d4229b04ad712106b484a024e64378d37
Bruno Seifert <128076381+brunoseifert@users.noreply.github.com>
2024-03-08 16:19:30 -0300

    feat: logical test 04

---
+++ 04.js
@@ -0,0 +1,5 @@
+// 4) Você está em uma sala com três interruptores, cada um conectado a uma lâmpada em uma sala diferente. Você não pode ver as lâmpadas da sala em que está, mas pode ligar e desligar os interruptores quantas vezes quiser. Seu objetivo é descobrir qual interruptor controla qual lâmpada.
+
+// Como você faria para descobrir, usando apenas duas idas até uma das salas das lâmpadas, qual interruptor controla cada lâmpada?
+
+// eu ligaria os interruptores na sequencia de cima para baixo, ligo a primeira lâmpada e esperaria esquentar, depois desligaria e ligaria outra, a lampada quente é a que eu liguei primeiro, a lampada que está ligado, foi a lampada que liguei por segundo, e a que não está ligada e nem quente foi a que não liguei. Descobrindo assim todas as lâmpadas correspondentes aos interruptores.
